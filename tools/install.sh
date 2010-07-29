011b79c0e10167fbf82ce44a1b919e4caad64905
Samson Ootoowak <ootoovak@gmail.com>
2010-07-29 13:56:24 +1200

    Update to include .gvim file for MacVim and GVim.

--- tools/install.sh
+++ tools/install.sh
@@ -4,8 +4,10 @@ then
   exit
 fi
 
+
 echo "Cloning Vim files..."
 /usr/bin/env git clone git@github.com:enspiral/vim-config.git ~/.vim
+
 
 echo "Looking for an existing vimrc config..."
 if [ -f ~/.vimrc ] || [ -h ~/.vimrc ]
@@ -18,8 +20,20 @@ fi
 echo "Using the Oh My Vim template file and adding it to ~/.vimrc"
 cp ~/.vim/templates/vimrc.template ~/.vimrc
 
-echo '         __                                  __           '
-echo '  ____  / /_     ____ ___  __  __  __   __  /_/____ ___  '
+
+echo "Looking for an existing gvimrc config..."
+if [ -f ~/.gvimrc ] || [ -h ~/.gvimrc ]
+then
+  echo "Found ~/.gvimrc. Backing up to ~/.gvimrc.pre-oh-my-vim";
+  cp ~/.gvimrc ~/.gvimrc.pre-oh-my-vim;
+  rm ~/.gvimrc
+fi
+
+echo "Using the Oh My Vim template file and adding it to ~/.vimrc"
+cp ~/.vim/templates/gvimrc.template ~/.gvimrc
+
+echo '         __                                  ___         '
+echo '  ____  / /_     ____ ___  __  __  __   __  /__/___ ___  '
 echo ' / __ \/ __ \   / __ `__ \/ / / /  | | / / /  / __ `__ \ '
 echo '/ /_/ / / / /  / / / / / / /_/ /   | |/ / /  / / / / / / '
 echo '\____/_/ /_/  /_/ /_/ /_/\__, /    |___/ /__/_/ /_/ /_/  '
